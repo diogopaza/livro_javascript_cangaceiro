4d2fe458a95fa03fb95529350002d18cc7e71eb8
diogopaza <diogopazacvel@gmail.com>
2019-03-04 11:26:42 -0300

    cap2.9

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -3,7 +3,7 @@ class Negociacao{
     
     constructor(data, quantidade, valor){
        
-        this._data = data
+        this._data = new Date( data.getTime())
         this._quantidade = quantidade
         this._valor = valor
         Object.freeze(this)
@@ -21,7 +21,8 @@ class Negociacao{
         return this._valor
     }
     get data(){
-        return this._data
+        return new Date( this._data.getTime())
+
     }
 
 
