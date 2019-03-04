2a348ccf4d6a9ce8d297ac43009e38b20cbe2ace
diogopaza <diogopazacvel@gmail.com>
2019-03-04 19:11:41 -0300

    3.3

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -2,8 +2,15 @@ class NegociacaoController{
 
 
     adiciona(event){
+       
         event.preventDefault()
-        console.log('chamei controller')
+        let inputData = document.getElementById('data')
+        let inputQuantidade = document.getElementById('quantidade')
+        let inputValor = document.getElementById('valor')
+        console.log(typeof(inputData.value))
+        console.log(inputQuantidade.value)
+        console.log(inputValor.value)
+
     }
 
 }
